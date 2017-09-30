5355873ce7bb1a069331d52fec3f9688a50b0be1
Matthew Woehlke <mwoehlke.floss@gmail.com>
2017-09-30 18:28:41 -0400

    Start adding action skeletons
    Add code to create several of the core actions that the application will
    require; particularly, those bits related to file I/O. (These are hooked
    up to slots, but the slots are not yet implemented. This also includes
    hooking up a recent files menu.) Also, add paths for settings
    persistence, and set up persistence of the splitter state.
    
    Basically, the application still doesn't do anything (besides persisting
    its UI state), but it's starting to take on much of its final visual shape.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,16 +1,97 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
+#include <KActionCollection>
+#include <KConfigGroup>
+#include <KSharedConfig>
+#include <KStandardAction>
+#include <KTextEdit>
+
 //-----------------------------------------------------------------------------
 MainWindow::MainWindow(QWidget *parent) :
     KXmlGuiWindow{parent},
-    ui{new Ui::MainWindow}
+    m_ui{new Ui::MainWindow}
 {
-    ui->setupUi(this);
+    m_ui->setupUi(this);
+
+    auto* const ac = actionCollection();
+    KStandardAction::open(this, qOverload<>(&MainWindow::open), ac);
+    KStandardAction::save(this, &MainWindow::save, ac);
+    KStandardAction::saveAs(this, qOverload<>(&MainWindow::saveAs), ac);
+    KStandardAction::quit(qApp, &QApplication::quit, ac);
+
+    m_recentFiles = KStandardAction::openRecent(
+        this, qOverload<QUrl const&>(&MainWindow::open), ac);
+
     setupGUI();
+    readConfig();
 }
 
 //-----------------------------------------------------------------------------
 MainWindow::~MainWindow()
 {
+}
+
+//-----------------------------------------------------------------------------
+void MainWindow::readConfig()
+{
+    auto const config = KSharedConfig::openConfig();
+
+    auto const cg = KConfigGroup{config, "MainWindow"};
+    m_ui->centralWidget->restoreState(
+        cg.readEntry("Splitter", QByteArray{}));
+
+    m_recentFiles->loadEntries(config->group("Recent Files"));
+}
+
+//-----------------------------------------------------------------------------
+void MainWindow::writeConfig()
+{
+    auto const config = KSharedConfig::openConfig();
+
+    auto cg = KConfigGroup{config, "MainWindow"};
+    cg.writeEntry("Splitter", m_ui->centralWidget->saveState());
+
+    m_recentFiles->saveEntries(KConfigGroup{config, "Recent Files"});
+
+    config->sync();
+}
+
+//-----------------------------------------------------------------------------
+bool MainWindow::queryClose()
+{
+    // TODO
+
+    writeConfig();
+    return true;
+}
+
+//-----------------------------------------------------------------------------
+void MainWindow::open()
+{
+    // TODO
+}
+
+//-----------------------------------------------------------------------------
+void MainWindow::open(QUrl const& url)
+{
+    // TODO
+}
+
+//-----------------------------------------------------------------------------
+void MainWindow::save()
+{
+    saveAs(m_location);
+}
+
+//-----------------------------------------------------------------------------
+void MainWindow::saveAs()
+{
+    // TODO
+}
+
+//-----------------------------------------------------------------------------
+void MainWindow::saveAs(QUrl const&)
+{
+    // TODO
 }
--- MainWindow.h
+++ MainWindow.h
@@ -3,7 +3,11 @@
 
 #include <KXmlGuiWindow>
 
+#include <QUrl>
+
 #include <memory>
+
+class KRecentFilesAction;
 
 namespace Ui {
 class MainWindow;
@@ -17,8 +21,25 @@ public:
     explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+    void readConfig();
+    void writeConfig();
+
+    virtual bool queryClose() override;
+
+protected slots:
+    void open();
+    void open(QUrl const&);
+
+    void save();
+    void saveAs();
+    void saveAs(QUrl const&);
+
 private:
-    std::unique_ptr<Ui::MainWindow> ui;
+    std::unique_ptr<Ui::MainWindow> m_ui;
+    QUrl m_location;
+    bool m_modified = false;
+
+    KRecentFilesAction* m_recentFiles;
 };
 
 #endif
